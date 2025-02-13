38267273f07080801dc485d2d2fafde30e70ad12
Ashik Parmar <ashikparmar5123@gmail.com>
2025-02-13 21:09:24 +0530

    localStorage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 
+import { useDispatch } from 'react-redux';
 import './App.css'
 import AddTodo from './components/AddTodo'
 import Todo from './components/Todo'
+import { resetTodos } from './redux/slices/todoSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const handleClearStorage = () => {
+    dispatch(resetTodos()); 
+  };
+  
 
   return (
     <>
-      <h2 className='font-extrabold text-3xl underline text-white'>Manage Your Todos</h2>
+      <button onClick={handleClearStorage} className="bg-red-500 absolute top-2 right-2 text-white px-4 py-2 rounded">
+        Clear All Todos
+      </button>
+      <h2 className='font-extrabold text-3xl mt-10 underline text-white'>Manage Your Todos</h2>
       <AddTodo/>
       <Todo/>
     </>
--- src/redux/slices/todoSlice.js
+++ src/redux/slices/todoSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit"
 
+
 const initialState = {
     todos: [{
         id:1, 
@@ -8,9 +9,21 @@ const initialState = {
         }]
 }
 
+const loadTodos = () => {
+    
+    let savedTodos = localStorage.getItem("todos");
+    savedTodos = JSON.parse(savedTodos);
+    
+    if(savedTodos && savedTodos.todos.length){
+        return savedTodos;
+    }
+
+    return initialState;
+}
+
 export const todoSlice = createSlice({
     name: 'todo',
-    initialState,
+    initialState: loadTodos(),
     reducers:{
         addTodo: (state, action)=>{
             const todo = {
@@ -19,26 +32,34 @@ export const todoSlice = createSlice({
                 complete: false,
             };
             state.todos.push(todo);
+            localStorage.setItem("todos", JSON.stringify(state));
         },
         removeTodo: (state, action)=>{
             state.todos = state.todos.filter((todo)=> todo.id !== action.payload)
+            localStorage.setItem("todos", JSON.stringify(state));
         },
         updateTodo: (state, action) => {
             const { id, newText } = action.payload;
             const todo = state.todos.find(todo => todo.id === id);
             if (todo) {
               todo.text = newText; 
+              localStorage.setItem("todos", JSON.stringify(state));
             }
         },
         toggleTodo: (state, action)=>{
             const todo = state.todos.find(todo => todo.id === action.payload);
             if (todo) {
               todo.complete = !todo.complete; 
+              localStorage.setItem("todos", JSON.stringify(state));
             }
         },
+        resetTodos: (state)=>{
+            state.todos = initialState.todos;
+            localStorage.removeItem("todos");
+        }
     }
 })
 
 
-export const {addTodo, removeTodo, updateTodo, toggleTodo} = todoSlice.actions;
+export const {addTodo, removeTodo, updateTodo, toggleTodo, resetTodos} = todoSlice.actions;
 export default todoSlice.reducer;
